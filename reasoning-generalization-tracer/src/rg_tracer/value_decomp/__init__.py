a24506a3a227ad79e8856ee5fb52dc21396ec999
InfiniteMalice <115899879+InfiniteMalice@users.noreply.github.com>
2025-12-08 05:10:00 -0500

    Refine overwatch heuristics and GRN coverage

---
+++ reasoning-generalization-tracer/src/rg_tracer/value_decomp/__init__.py
@@ -0,0 +1,25 @@
+"""Value decomposition utilities bridging deep values and shallow preferences."""
+
+from .deep_value_decomp import (
+    DeepValueVector,
+    ShallowFeatureVector,
+    ValueDecompResult,
+    analyze_output_deep_values,
+    analyze_output_shallow_features,
+    compute_dvgr,
+    decompose_score,
+    parse_user_deep_values,
+    parse_user_shallow_prefs,
+)
+
+__all__ = [
+    "DeepValueVector",
+    "ShallowFeatureVector",
+    "ValueDecompResult",
+    "analyze_output_deep_values",
+    "analyze_output_shallow_features",
+    "compute_dvgr",
+    "decompose_score",
+    "parse_user_deep_values",
+    "parse_user_shallow_prefs",
+]
